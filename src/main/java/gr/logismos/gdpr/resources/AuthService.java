b7cdcdaeb2ecfd1c0d6a061b2894bd8952a62c6c
northdigital <giotis@email.com>
2017-10-18 14:48:19 +0300

    commit

--- src/main/java/gr/logismos/gdpr/resources/AuthService.java
+++ src/main/java/gr/logismos/gdpr/resources/AuthService.java
@@ -1,6 +1,7 @@
 package gr.logismos.gdpr.resources;
 
 import gr.logismos.gdpr.models.UserCredentials;
+import gr.logismos.x509certificatevalidator.X509CertificateValidator;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMReader;
 import org.bouncycastle.openssl.PasswordFinder;
@@ -12,6 +13,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.security.KeyStore;
 import java.security.Security;
 import java.security.cert.X509Certificate;
 
@@ -21,17 +23,23 @@ public class AuthService {
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   @Path("/authenticate")
-  public UserCredentials authenticate(UserCredentials userCredentials) {
+  public UserCredentials authenticate(UserCredentials userCredentials) throws Exception {
 
-    X509Certificate cert = null;
+    X509Certificate x509Certificate = null;
     Object o = null;
 
     try {
       StringReader reader = new StringReader(userCredentials.pem);
-      PEMReader pr = new PEMReader(reader);
-      cert = (X509Certificate) pr.readObject();
-      pr.close();
-    } catch (IOException e) {
+      PEMReader pemReader = new PEMReader(reader);
+      x509Certificate = (X509Certificate) pemReader.readObject();
+      pemReader.close();
+
+      X509CertificateValidator x509CertificateValidator = new X509CertificateValidator();
+      KeyStore keyStore = x509CertificateValidator.loadKeyStoreFromFile(
+        "D:\\Projects\\X509CertificateValidator\\ssl\\keys2.jks", "sporades");
+      boolean isValid = x509CertificateValidator.isValidCertificate(x509Certificate,keyStore);
+      System.out.println(isValid);
+    } catch (Exception e) {
       e.printStackTrace();
     }
 
